81ff1447974f1088b0c9c0351d4672c411b1d3ed
Nick Vandenberge <nvandenberge14@gmail.com>
2021-02-15 21:46:52 -0500

    express server running

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
