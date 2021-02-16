1c7e4fa72b3a6472377280c472194a3d9a2f0e75
Nick Vandenberge <nvandenberge14@gmail.com>
2021-02-15 21:57:50 -0500

    added html routes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 // ================================================================================
 
 // require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
